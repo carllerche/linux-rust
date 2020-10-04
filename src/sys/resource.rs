e479774592597a20c38ca298fb609bf646afa692
LMJW <heysuperming@gmail.com>
2020-10-04 14:22:45 +1100

    Fixing test

--- src/sys/resource.rs
+++ src/sys/resource.rs
@@ -2,73 +2,34 @@
 use cfg_if::cfg_if;
 use std::mem;
 
-#[cfg(any(target_os = "linux"))]
-use libc::{__rlimit_resource_t, rlimit, RLIM_INFINITY};
-
-#[cfg(any(
-    target_os = "freebsd",
-    target_os = "openbsd",
-    target_os = "netbsd",
-    target_os = "macos",
-    target_os = "ios",
-    target_os = "android",
-    target_os = "dragonfly",
-    target_os = "bitrig",
-))]
-use libc::{c_int, rlimit, RLIM_INFINITY};
-
 use crate::errno::Errno;
 use crate::Result;
 
 pub use libc::rlim_t;
 
 cfg_if! {
-    if #[cfg(target_os = "linux")]{
-        if #[cfg(target_env = "gnu")]{
-            libc_enum!{
-                #[repr(u32)]
-                pub enum Resource {
-                    /// See detail of each Resource https://man7.org/linux/man-pages/man2/getrlimit.2.html
-                    RLIMIT_AS,
-                    RLIMIT_CORE,
-                    RLIMIT_CPU,
-                    RLIMIT_DATA,
-                    RLIMIT_FSIZE,
-                    RLIMIT_LOCKS,
-                    RLIMIT_MEMLOCK,
-                    RLIMIT_MSGQUEUE,
-                    RLIMIT_NICE,
-                    RLIMIT_NOFILE,
-                    RLIMIT_NPROC,
-                    RLIMIT_RSS,
-                    RLIMIT_RTPRIO,
-                    RLIMIT_RTTIME,
-                    RLIMIT_SIGPENDING,
-                    RLIMIT_STACK,
-                }
-            }
-        }else{
-            libc_enum!{
-                #[repr(i32)]
-                pub enum Resource {
-                    /// See detail of each Resource https://man7.org/linux/man-pages/man2/getrlimit.2.html
-                    RLIMIT_AS,
-                    RLIMIT_CORE,
-                    RLIMIT_CPU,
-                    RLIMIT_DATA,
-                    RLIMIT_FSIZE,
-                    RLIMIT_LOCKS,
-                    RLIMIT_MEMLOCK,
-                    RLIMIT_MSGQUEUE,
-                    RLIMIT_NICE,
-                    RLIMIT_NOFILE,
-                    RLIMIT_NPROC,
-                    RLIMIT_RSS,
-                    RLIMIT_RTPRIO,
-                    RLIMIT_RTTIME,
-                    RLIMIT_SIGPENDING,
-                    RLIMIT_STACK,
-                }
+    if #[cfg(all(target_os = "linux", target_env = "gnu"))]{
+        use libc::{__rlimit_resource_t, rlimit, RLIM_INFINITY};
+        libc_enum!{
+            #[repr(u32)]
+            pub enum Resource {
+                /// See detail of each Resource https://man7.org/linux/man-pages/man2/getrlimit.2.html
+                RLIMIT_AS,
+                RLIMIT_CORE,
+                RLIMIT_CPU,
+                RLIMIT_DATA,
+                RLIMIT_FSIZE,
+                RLIMIT_LOCKS,
+                RLIMIT_MEMLOCK,
+                RLIMIT_MSGQUEUE,
+                RLIMIT_NICE,
+                RLIMIT_NOFILE,
+                RLIMIT_NPROC,
+                RLIMIT_RSS,
+                RLIMIT_RTPRIO,
+                RLIMIT_RTTIME,
+                RLIMIT_SIGPENDING,
+                RLIMIT_STACK,
             }
         }
     }else if #[cfg(any(
@@ -80,7 +41,10 @@ cfg_if! {
         target_os = "android",
         target_os = "dragonfly",
         target_os = "bitrig",
+        target_os = "linux", // target_env != "gnu"
     ))]{
+        use libc::{c_int, rlimit, RLIM_INFINITY};
+
         libc_enum! {
             #[repr(i32)]
             pub enum Resource {
@@ -98,31 +62,34 @@ cfg_if! {
                 #[cfg(target_os = "freebsd")]
                 RLIMIT_KQUEUES,
 
-                #[cfg(any(target_os = "android"))]
+                #[cfg(any(target_os = "android", target_os = "linux"))]
                 RLIMIT_LOCKS,
 
-                #[cfg(any(target_os = "android", target_os = "freebsd", target_os = "openbsd"))]
+                #[cfg(any(target_os = "android", target_os = "freebsd", target_os = "openbsd", target_os = "linux"))]
                 RLIMIT_MEMLOCK,
 
-                #[cfg(any(target_os = "android"))]
+                #[cfg(any(target_os = "android", target_os = "linux"))]
                 RLIMIT_MSGQUEUE,
 
-                #[cfg(any(target_os = "android"))]
+                #[cfg(any(target_os = "android", target_os = "linux"))]
                 RLIMIT_NICE,
 
-                #[cfg(any(target_os = "android", target_os = "freebsd", target_os = "openbsd"))]
+                #[cfg(any(target_os = "android", target_os = "freebsd", target_os = "openbsd", target_os = "linux"))]
                 RLIMIT_NPROC,
 
                 #[cfg(target_os = "freebsd")]
                 RLIMIT_NPTS,
 
-                #[cfg(any(target_os = "android", target_os = "freebsd", target_os = "openbsd"))]
+                #[cfg(any(target_os = "android", target_os = "freebsd", target_os = "openbsd", target_os = "linux"))]
                 RLIMIT_RSS,
 
-                #[cfg(any(target_os = "android"))]
+                #[cfg(any(target_os = "android", target_os = "linux"))]
                 RLIMIT_RTPRIO,
 
-                #[cfg(any(target_os = "android"))]
+                #[cfg(any(target_os = "linux"))]
+                RLIMIT_RTTIME,
+
+                #[cfg(any(target_os = "android", target_os = "linux"))]
                 RLIMIT_SIGPENDING,
 
                 #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
@@ -166,7 +133,7 @@ cfg_if! {
 pub fn getrlimit(resource: Resource) -> Result<(Option<rlim_t>, Option<rlim_t>)> {
     let mut rlim = mem::MaybeUninit::<rlimit>::uninit();
 
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", target_env = "gnu"))]
     let res =
         unsafe { libc::getrlimit(resource as __rlimit_resource_t, rlim.as_mut_ptr() as *mut _) };
     #[cfg(any(
@@ -178,6 +145,7 @@ pub fn getrlimit(resource: Resource) -> Result<(Option<rlim_t>, Option<rlim_t>)>
         target_os = "android",
         target_os = "dragonfly",
         target_os = "bitrig",
+        target_os = "linux", // target_env != "gnu"
     ))]
     let res = unsafe { libc::getrlimit(resource as c_int, rlim.as_mut_ptr() as *mut _) };
 
@@ -197,9 +165,11 @@ pub fn getrlimit(resource: Resource) -> Result<(Option<rlim_t>, Option<rlim_t>)>
 /// # Parameters
 ///
 /// * `resource`: The [`Resource`] that we want to set the limits of.
-/// * `soft_limit`: The value that the kernel enforces for the corresponding resource.
-/// * `hard_limit`: The ceiling for the soft limit. Must be lower or equal to the current hard limit
-///   for non-root users.
+/// * `soft_limit`: The value that the kernel enforces for the corresponding
+///   resource. Note: `None` input will be replaced by constant `RLIM_INFINITY`.
+/// * `hard_limit`: The ceiling for the soft limit. Must be lower or equal to
+///   the current hard limit for non-root users. Note: `None` input will be
+///   replaced by constant `RLIM_INFINITY`.
 ///
 /// # Examples
 ///
@@ -225,7 +195,7 @@ pub fn setrlimit(
     rlim.rlim_cur = soft_limit.unwrap_or(RLIM_INFINITY);
     rlim.rlim_max = hard_limit.unwrap_or(RLIM_INFINITY);
 
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", target_env = "gnu"))]
     let res = unsafe { libc::setrlimit(resource as __rlimit_resource_t, &rlim as *const _) };
     #[cfg(any(
         target_os = "freebsd",
@@ -236,6 +206,7 @@ pub fn setrlimit(
         target_os = "android",
         target_os = "dragonfly",
         target_os = "bitrig",
+        target_os = "linux", // target_env != "gnu"
     ))]
     let res = unsafe { libc::setrlimit(resource as c_int, &rlim as *const _) };
     Errno::result(res).map(|_| ())
--- test/test_resource.rs
+++ test/test_resource.rs
@@ -34,21 +34,21 @@ pub fn test_resource_limits_stack() {
     let (mut soft_limit, hard_limit) = getrlimit(Resource::RLIMIT_STACK).unwrap();
     let orig_limit = (soft_limit, hard_limit);
 
-    soft_limit = hard_limit;
+    soft_limit = hard_limit.or(Some(10000));
     setrlimit(Resource::RLIMIT_STACK, soft_limit, hard_limit).unwrap();
 
     let limit2 = getrlimit(Resource::RLIMIT_STACK).unwrap();
-    // assert_eq!(soft_limit, limit2.0);
-    // assert_eq!(hard_limit, limit2.1);
+    assert_eq!(soft_limit, limit2.0);
+    assert_eq!(hard_limit, limit2.1);
 
     setrlimit(Resource::RLIMIT_STACK, orig_limit.0, orig_limit.1).unwrap();
 
     let final_limit = getrlimit(Resource::RLIMIT_STACK).unwrap();
-    // assert_eq!(orig_limit.0, final_limit.0);
-    // assert_eq!(orig_limit.1, final_limit.1);
+    assert_eq!(orig_limit.0, final_limit.0);
+    assert_eq!(orig_limit.1, final_limit.1);
 
-    assert_eq!(
-        format!("softlimit-{:?}, hardlimit-{:?}, limit2.0-{:?}, limit2.1-{:?}, final_limit.0-{:?}, final_limit.0-{:?}"
-        , soft_limit, hard_limit, limit2.0, limit2.1, final_limit.0, final_limit.1),
-        "");
+    // assert_eq!(
+    //     format!("softlimit-{:?}, hardlimit-{:?}, limit2.0-{:?}, limit2.1-{:?}, final_limit.0-{:?}, final_limit.0-{:?}"
+    //     , soft_limit, hard_limit, limit2.0, limit2.1, final_limit.0, final_limit.1),
+    //     "");
 }
